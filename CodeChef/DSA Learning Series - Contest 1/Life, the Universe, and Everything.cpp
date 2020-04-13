e8e88f8bee7af8cf62b7999b1999fc3ad81a4f33
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-04-13 23:16:34 +0530

    c

---
+++ CodeChef/DSA Learning Series - Contest 1/Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define loop(typeofx, x, start, end) for(typeofx x = start; x < end; ++ x)
+#define listTraverse(datatypeoflist, x, startList, endList) for(list<datatypeoflist>::iterator x = startList; x != endList; ++ x)
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    if(n==42) return 0;
+    cout<<n<<endl;
+    
+}

6f1e3e6d79d2966a2a145efebf046c5a100ff84b
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-04-19 17:37:18 +0530

    cp

---
+++ AtCoder/AtCoder Beginner Contest 163/A - Circle Pond.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define loop(typeofx, x, start, end) for (typeofx x = start; x < end; ++x)
+#define listTraverse(datatypeoflist, x, startList, endList) for (list<datatypeoflist>::iterator x = startList; x != endList; ++x)
+#define ll long long int
+#define l long int
+#define pb push_back
+#define MOD 1000000007
+
+int main()
+{
+
+    int r;
+    cin >> r;
+
+    float c;
+    c = (2 * 22 * r) / 7;
+    cout << c;
+}

57aa9a9aecbdc2d98551797dcba616fa597c018a
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-04-21 04:12:52 +0530

    cp

--- CodeChef/April Cook-Off 2020 Division 2/Matrix Decomposition.cpp
+++ CodeChef/April Cook-Off 2020 Division 2/Matrix Decomposition.cpp
@@ -15,7 +15,7 @@ int main()
     cin >> t;
     ll MOD = 1000000007;
 
-    loop(int, i, 0, t)
+    while(t--)
     {
         int n;
         unsigned ll a;
@@ -23,30 +23,39 @@ int main()
         unsigned ll p[n] = {1};
         unsigned ll sum = 0;
 
-        unsigned ll m[n][n] = {
-            {a, a},
-            {a, a}};
+        unsigned ll m[n][n];
+
+        loop(int, g, 0, n)
+        {
+            loop(int, h, 0, n)
+            {
+                m[g][h] = a;
+            }
+        }
 
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j < i; j++)
             {
-                p[i - 1] *= m[j][n - i + 1];
-                m[j][n - i + 1] = 0;
+                if (!m[j][n - i + 1] == 0)
+                {
+                    p[i - 1] *= m[j][n - i + 1];
+                    m[j][n - i + 1] = 0;
+                }
             }
             for (int j = 1; j <= i; j++)
             {
-                p[i - 1] *= m[i][n - i + j];
-                m[i][n - i + j] = 0;
+                if (!m[i][n - i + j] == 0)
+                {
+                    p[i - 1] *= m[i][n - i + j];
+                    m[i][n - i + j] = 0;
+                }
             }
             loop(int, g, 0, n)
             {
                 loop(int, h, 0, n)
                 {
-                    if (!m[g][h] == 0)
-                    {
-                        m[g][h] *= p[i - 1];
-                    }
+                    m[g][h] *= p[i - 1];
                 }
             }
 
@@ -55,3 +64,67 @@ int main()
         cout << sum << endl;
     }
 }
+
+// RAHUL's SOLUTION
+
+// #include <iostream>
+// using namespace std;
+
+// #define ll long long int
+
+
+// long long int powerr(long long int a, int p)
+// {
+//     if (p == 1)
+//     {
+//         return a;
+//     }
+//     else if (p % 2 == 0)
+//     {
+//         long long int qwe = powerr(a, (p / 2));
+//         qwe = qwe * qwe;
+//         qwe = qwe % 1000000007;
+//         return qwe;
+//     }
+//     else
+//     {
+//         long long int qwe = powerr(a, (p / 2));
+//         qwe = qwe * qwe;
+//         qwe = qwe % 1000000007;
+//         qwe = qwe * a;
+//         qwe = qwe % 1000000007;
+//         return qwe;
+//     }
+// }
+// int solve(long long int a, int p, long long int pf)
+// {
+//     a = a * pf;
+//     a = a % 1000000007;
+//     long long int ans = powerr(a, p);
+//     return ans;
+// }
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     for (int u = 1; u <= t; u++)
+//     {
+//         ll n, a;
+//         cin >> n >> a;
+//         ll arr[n + 1];
+//         ll sum = 0;
+//         ll productfactor = 1;
+//         for (int i = 1; i <= n; i++)
+//         {
+//             ll yyyyy = solve(a, (2 * i) - 1, productfactor);
+//             sum = sum + yyyyy;
+//             sum = sum % 1000000007;
+//             productfactor *= yyyyy;
+//             productfactor = productfactor % 1000000007;
+//         }
+
+//         cout << sum << endl;
+//     }
+
+//     return 0;
+// }

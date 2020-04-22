30091421a535e3b644923e9a35f2207c07af6854
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-04-22 20:17:24 +0530

    cp

--- RandomCodes/char.cpp
+++ RandomCodes/char.cpp
@@ -8,10 +8,35 @@ using namespace std;
 #define l long int
 #define pb push_back
 #define MOD 1000000007
-
+int sumarray(int a[], int sizeofa, int b[], int sizeofb)
+{
+    int sizeofc = sizeofa > sizeofb ? (sizeofa + 1) : (sizeofb + 1);
+    int c[sizeofc];
+    int frontpart = 0;
+    for (int i = sizeofa - 1; i >= 0; i--)
+    {
+        c[i] = (frontpart + a[i] + b[i]) % 10;
+        frontpart = ((a[i] + b[i]) - ((a[i] + b[i]) % 10)) % 10;
+    }
+    if (i == 0 && sizeofb > sizeofa)
+    {
+    }
+}
 int main()
 {
-    string s = "12345";
-    int a = s[0]-'0';
-    cout<<a;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int m;
+    cin >> m;
+    int b[m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    sumarray(a, n, b, m);
 }

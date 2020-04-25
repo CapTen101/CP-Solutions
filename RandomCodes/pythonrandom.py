96be85b92ca931fb71bcf1cc36254f11f1575471
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-04-25 18:27:18 +0530

    cp

--- RandomCodes/pythonrandom.py
+++ RandomCodes/pythonrandom.py
@@ -1,2 +1,5 @@
-n=3
-print(n)
+n=int(input('Enter the number: ')) #this takes an integer input n
+if n%2==0:                         #checks for even number
+    print('number is even')
+else:                              #checks for odd number 
+    print('number is odd')

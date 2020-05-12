1b3fae26812567ad7cbc0551b56e10a3211c812f
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-05-12 19:57:43 +0530

    cp

--- MyCodeTemplates/QuickSort.cpp
+++ MyCodeTemplates/QuickSort.cpp
@@ -8,12 +8,39 @@ using namespace std;
 #define ull unsigned long long int
 #define l long int
 #define ul unsigned long int
-#define start int t;cin>>t; while(t--)
+// #define start int t;cin>>t; while(t--)
 const ll mod = 1000000007;
+
+int Partition(int *A, int start, int end)
+{
+
+    int pivot = A[end];
+    int pIndex = start;
+
+    for (int i = start; i < end; i++)
+    {
+        if (A[i] <= pivot)
+        {
+            swap(A[pIndex], A[i]);
+            pIndex++;
+        }
+    }
+    swap(pIndex, pivot);
+    return pIndex;
+}
+
+void QuickSort(int *A, int start, int end)
+{
+    int pIndex = Partition(A, start, end);
+    QuickSort(A, start, pIndex - 1);
+    QuickSort(A, pIndex + 1, end);
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
+    int arr[9] = {1, 5, 2, 9, 4, 4, 0, 6, 2};
+    QuickSort(arr, 0, 8);
 
-    
+    loop(int, i, 0, 8) cout << arr[i] << " ";
 }

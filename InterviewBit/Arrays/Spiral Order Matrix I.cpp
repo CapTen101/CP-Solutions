4a208ade277082ed207d3bc050041697a501dd00
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-05-14 12:38:35 +0530

    cp

---
+++ InterviewBit/Arrays/Spiral Order Matrix I.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define loop(typeofx, x, start, end) for (typeofx x = start; x < end; x++)
+#define listTraverse(datatypeoflist, x, startList, endList) for (list<datatypeoflist>::iterator x = startList; x != endList; ++x)
+#define ll long long int
+#define ull unsigned long long int
+#define l long int
+#define ul unsigned long int
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+const ll mod = 1000000007;
+
+class Solution
+{
+    vector<int> Solution::spiralOrder(const vector<vector<int>> &A);
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+}

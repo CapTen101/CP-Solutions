51c42162f1bb877cdcf60b86db966819b103f942
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-05-19 00:59:43 +0530

    cp

--- RandomCodes/DP/RecursiveKnapsackMemoization.cpp
+++ RandomCodes/DP/RecursiveKnapsackMemoization.cpp
@@ -14,24 +14,29 @@ using namespace std;
     while (t--)
 const ll mod = 1000000007;
 
-int knapsack(int wt[], int val[], int size, int capacity)
+int t[9][101];
+int knapsackMemoization(int wt[], int val[], int size, int capacity)
 {
     if (size == 0 || capacity == 0)
         return 0;
 
+    if (t[size][capacity] != -1)
+        return t[size][capacity];
+
     if (wt[size - 1] <= capacity)
-        return max(val[size - 1] + knapsack(wt, val, size - 1, capacity - wt[size - 1]), knapsack(wt, val, size - 1, capacity));
+        return t[size][capacity] = max(val[size - 1] + knapsackMemoization(wt, val, size - 1, capacity - wt[size - 1]), knapsackMemoization(wt, val, size - 1, capacity));
 
     else
-        return knapsack(wt, val, size - 1, capacity);
+        return t[size][capacity] = knapsackMemoization(wt, val, size - 1, capacity);
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
 
+    memset(t, -1, sizeof(t));
     int wt[8] = {1, 2, 3, 4, 5, 6, 7, 8};
     int val[8] = {500, 30, 50, 344, 43, 93, 900, 3000};
     int capacity = 100;
-    cout << knapsack(wt, val, 8, capacity);
+    cout << knapsackMemoization(wt, val, 8, capacity) << endl;
 }

b44449d82f8ee27ce31e52720ea9f49ffd6cb40c
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-05-21 20:36:52 +0530

    cp

---
+++ RandomCodes/DP/RecursiveEqualSumPartition.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define loop(typeofx, x, start, end) for (typeofx x = start; x < end; x++)
+#define listTraverse(datatypeoflist, x, startList, endList) for (list<datatypeoflist>::iterator x = startList; x != endList; ++x)
+#define ll long long int
+#define ull unsigned long long int
+#define l long int
+#define ul unsigned long int
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+const ll mod = 1000000007;
+
+bool subsetsum(int arr[], int n, int sum)
+{
+    if (sum == 0)
+        return true;
+    if (n == 0 && sum != 0)
+        return false;
+
+    if (arr[n - 1] <= sum)
+        return subsetsum(arr, n - 1, sum - arr[n - 1]) || subsetsum(arr, n - 1, sum);
+    else
+        return subsetsum(arr, n - 1, sum);
+}
+
+bool equalsumpartition(int arr[], int n)
+{
+    int totalsum = 0;
+    loop(int, i, 0, n)
+        totalsum += arr[i];
+    int halfsum = totalsum / 2;
+
+    if (totalsum % 2 != 0)
+        return false;
+    else
+        return subsetsum(arr, n, halfsum);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int arr[2] = {2, 2};
+    int sum = 2;
+    bool ans = equalsumpartition(arr, 2);
+
+    if (ans)
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+}
---
+++ RandomCodes/DP/TabulationCountSubsetSum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define loop(typeofx, x, start, end) for (typeofx x = start; x < end; x++)
+#define listTraverse(datatypeoflist, x, startList, endList) for (list<datatypeoflist>::iterator x = startList; x != endList; ++x)
+#define ll long long int
+#define ull unsigned long long int
+#define l long int
+#define ul unsigned long int
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+const ll mod = 1000000007;
+
+int t[6][12];
+int countsubsetsum(int arr[], int n, int sum)
+{
+    loop(int, i, 0, n + 1)
+    {
+        loop(int, j, 0, sum + 1)
+        {
+            if (i == 0)
+                t[i][j] = 0;
+            if (j == 0)
+                t[i][j] = 1;
+        }
+    }
+
+    loop(int, i, 1, n + 1)
+    {
+        loop(int, j, 1, sum + 1)
+        {
+            if (arr[i - 1] <= sum)
+                t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j];
+            else
+                t[i][j] = t[i - 1][j];
+        }
+    }
+
+    return t[n][sum];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int arr[5] = {2, 3, 7, 0, 11};
+    int sum = 11;
+    int ans = countsubsetsum(arr, 5, sum);
+    cout << ans << endl;
+}

e160d6fc687ebdb48e6ab47100ec0c2e2fe4214b
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-07-20 23:20:48 +0530

    cp

--- GFG/PlacementPreparationCourse/Arrays/Rearrange an array with O(1) extra space.cpp
+++ GFG/PlacementPreparationCourse/Arrays/Rearrange an array with O(1) extra space.cpp
@@ -12,7 +12,7 @@ void arrange(long long arr[], int n)
     // Your code here
     for (int i = 0; i < n; i++)
     {
-        arr[i] += arr[arr[i]] * n;
+        arr[i] += (arr[arr[i]] % n) * n;
     }
 
     for (int i = 0; i < n; i++)

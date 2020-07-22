4e988b25321dfeb3a6805b699b172d234b440cc5
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-07-22 21:38:18 +0530

    cp

---
+++ ResourcesFromYoutube/Aditya Verma/Binary Search/Rotation Count in Rotated Sorted array.cpp
@@ -0,0 +1,23 @@
+/*
+Problem Statement: https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
+*/
+
+// no. of time sorted array is rotated is "index of the smallest element" {CLOCKWISE ROATION}
+// no. of time sorted array is rotated is "(length of array - index of minimum element) % length of array" {ANTI-CLOCKWISE ROATION}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    start
+    {
+    }
+}

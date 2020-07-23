d87f0177b4d261b39d51e51fd6722b6edacedc82
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-07-23 22:21:25 +0530

    cp

--- ResourcesFromYoutube/Aditya Verma/Binary Search/Rotation Count in Rotated Sorted array.cpp
+++ ResourcesFromYoutube/Aditya Verma/Binary Search/Rotation Count in Rotated Sorted array.cpp
@@ -5,19 +5,113 @@ Problem Statement: https://www.geeksforgeeks.org/find-rotation-count-rotated-sor
 // no. of time sorted array is rotated is "index of the smallest element" {CLOCKWISE ROATION}
 // no. of time sorted array is rotated is "(length of array - index of minimum element) % length of array" {ANTI-CLOCKWISE ROATION}
 
+// below is for clockwise rotation:
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #define start \
+//     int t;    \
+//     cin >> t; \
+//     while (t--)
+
+// int bin(int arr[], int n)
+// {
+//     int l = 0, r = n - 1;
+
+//     while (l <= r)
+//     {
+//         if (arr[l] < arr[r])
+//         {
+//             return l;
+//         }
+
+//         int mid = l + (r - l) / 2;
+
+//         int next = (mid + 1) % n;
+//         int prev = (mid + n - 1) % n;
+
+//         if (arr[mid] >= arr[l])
+//         {
+
+//             if (arr[mid] > arr[next])
+
+//                 return arr[next];
+
+//             l = mid + 1;
+//         }
+//         else
+//         {
+
+//             if (arr[mid] < arr[prev])
+
+//                 return arr[mid];
+
+//             r = mid;
+//         }
+//     }
+
+//     return arr[l];
+// }
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+
+//     start
+//     {
+//         int n;
+//         cin >> n;
+//         int arr[n];
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             cin >> arr[i];
+//         }
+
+//         cout << bin(arr, n) << endl;
+//     }
+// }
+
+/*
+ACCEPTED SOLUTION
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+int pivotind(long long int arr[], long int st, long int end, long int n)
+{
+    if (n == 1)
+        return 0;
+    if (n == 2)
+    {
+        if (arr[0] > arr[1])
+            return 1;
+        else
+            return 0;
+    }
 
-#define start \
-    int t;    \
-    cin >> t; \
-    while (t--)
+    for (long i = 0; i < n; i++)
+    {
+        if (arr[0] < arr[n - 1])
+            return 0;
+        if (i > 0 && arr[i] <= arr[(i + 1) % n] && arr[i] < arr[i - 1])
+            return i;
+    }
 
+    return -1;
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-
-    start
+    int t;
+    cin >> t;
+    while (t--)
     {
+        long int n;
+        cin >> n;
+        long long int a[n];
+        for (long i = 0; i < n; i++)
+            cin >> a[i];
+        cout << pivotind(a, 0, n - 1, n) << endl;
     }
+    return 0;
 }
---
+++ ResourcesFromYoutube/Aditya Verma/Binary Search/Search in a Rotated Array.cpp
@@ -0,0 +1,41 @@
+/*
+Problem Statement: https://www.geeksforgeeks.org/search-an-element-in-a-sorted-and-pivoted-array/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define loop(typeofx, x, start, end) for (typeofx x = start; x < end; x++)
+#define ll long long int
+#define ull unsigned long long int
+#define l long int
+#define ul unsigned long int
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+const ll mod = 1000000007;
+
+int bin_search(int arr[], int n)
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    start
+    {
+        int n, k;
+        cin >> n;
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        cin >> k;
+    }
+}

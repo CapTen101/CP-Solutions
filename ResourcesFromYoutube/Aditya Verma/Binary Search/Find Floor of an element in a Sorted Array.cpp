0b0a8d3db85bdb3b541c9f6fe4055ae5902f6d09
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-07-25 23:02:48 +0530

    cp

--- ResourcesFromYoutube/Aditya Verma/Binary Search/Find Floor of an element in a Sorted Array.cpp
+++ ResourcesFromYoutube/Aditya Verma/Binary Search/Find Floor of an element in a Sorted Array.cpp
@@ -14,10 +14,30 @@ int search(long long arr[], int n, int x)
 {
     int l = 0;
     int r = n - 1;
+    int res = -1;
 
-    while (l < r)
+    if (arr[r] < x)
+        return r;
+
+    while (l <= r)
     {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        else if (arr[mid] < x)
+        {
+            res = mid;
+            l = mid + 1;
+        }
+        else if (arr[mid] > x)
+        {
+            r = mid - 1;
+        }
     }
+
+    return -1;
 }
 
 int main()
@@ -36,6 +56,6 @@ int main()
             cin >> arr[i];
         }
 
-        cout << search(arr, n, x);
+        cout << search(arr, n, x) << endl;
     }
 }

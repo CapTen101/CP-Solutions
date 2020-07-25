27f251c294d00a6fdc5586a866f6f264c1904c18
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-07-26 02:24:30 +0530

    cp

---
+++ ResourcesFromYoutube/Aditya Verma/Binary Search/Binary Search on Answer Concept.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+// #define loop(typeofx, x, start, end) for (typeofx x = start; x < end; x++)
+// #define ll long long int
+// #define ull unsigned long long int
+// #define l long int
+// #define ul unsigned long int
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+// const ll mod = 1000000007;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+}

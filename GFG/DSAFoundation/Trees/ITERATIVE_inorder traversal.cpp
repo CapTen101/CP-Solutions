086cea63978dc13cb681db83443a3b7d448cd557
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-08-15 04:16:10 +0530

    cp

---
+++ GFG/DSAFoundation/Trees/ITERATIVE_inorder traversal.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+struct Node
+{
+    int data;
+    Node *right;
+    Node *left;
+};
+
+void inorder(Node *root)
+{
+    Node *current = root;
+    stack<Node *> s;
+
+    while (current != NULL || s.empty() == false)
+    {
+        while (current != NULL)
+        {
+            s.push(current);
+            current = current->left; // go all the way to left bottom.
+        }
+        current = s.top();        // take the top most element of stack(bottom omst element of the tree)
+        cout << current->data;    // print it
+        s.pop();                  // remove it
+        current = current->right; // shift current to right and repeat the same process
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+}
---
+++ GFG/DSAFoundation/Trees/ITERATIVE_preorder traversal.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+struct Node
+{
+    int data;
+    Node *right;
+    Node *left;
+};
+
+void inorder(Node *root)
+{
+    Node *current = root;
+    stack<Node *> s;
+
+    while (current != NULL || s.empty() == false)
+    {
+        while (current != NULL)
+        {
+            cout << current->data; // print it first
+            s.push(current);
+            current = current->left; // go all the way to left bottom.
+        }
+
+        current = s.top();        // take the top most element of stack(bottom most element of the tree)
+        s.pop();                  // remove it
+        current = current->right; // shift current to right and repeat the same process
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+}

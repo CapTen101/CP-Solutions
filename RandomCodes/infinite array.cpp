9ea5665f52d96efc5f918641b8588ebeb2a373b0
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-08-20 02:13:34 +0530

    cp

---
+++ RandomCodes/infinite array.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int k = 1;
+
+    string num = "";
+
+    while (num.length() <= n)
+    {
+        string test = to_string(k);
+        num.append(test);
+    }
+
+    cout << num[n - 1];
+}

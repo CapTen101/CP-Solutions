7867603de93032e9ac45850fb89b2a40adc4480d
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-09-02 11:54:31 +0530

    cp

---
+++ InterviewBit/LRU Cache.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+class LRUCache;
+
+LRUCache::LRUCache(int capacity)
+{
+    map<int, int> lrumap(2);
+}
+
+int LRUCache::get(int key)
+{
+}
+
+void LRUCache::set(int key, int value)
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+}

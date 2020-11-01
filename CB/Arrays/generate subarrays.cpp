1c6f2f9ade4ce6e8e9b59a9640328b10e283321b
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-11-01 23:12:52 +0530

    cp

---
+++ CB/Arrays/generate subarrays.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Generate all sub-arrays
+void subarray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    subarray(arr, n);
+}

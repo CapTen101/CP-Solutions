aa80438ed06ecb1524a04fb2b48968d032ad92ea
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-11-26 22:38:49 +0530

    cp

--- CB/Arrays/maximum sum subarray.cpp
+++ CB/Arrays/maximum sum subarray.cpp
@@ -17,13 +17,22 @@ int main()
     int currentsum = 0;
 
     vector<int> prefixArray(n, 0);
+    prefixArray[0] = 0;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
         prefixArray[i] = prefixArray[i - 1] + arr[i];
     }
 
     FOR(i, 0, n)
     {
+        FOR(j, i, n)
+        {
+            currentsum = 0;
+            currentsum = prefixArray[j] - prefixArray[i - 1];
+            currentsum = max(currentsum, maxsum);
+        }
     }
+
+    cout << maxsum << endl;
 }

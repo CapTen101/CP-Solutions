45a42b27e73a06fa20e24072c9a0afec89582117
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-12-01 13:36:33 +0530

    cp

--- CB/strings/4. custom_strtok.cpp
+++ CB/strings/4. custom_strtok.cpp
@@ -13,7 +13,56 @@ using namespace std;
     while (t--)
 const ll mod = 1000000007;
 
+char *mystrtok(char *str, char DELIM)
+{
+    // this is the static pointer pointing to every token
+    static char *input = NULL;
+
+    // if input is not empty, assign the traversal variable to the start of the input
+    if (str != NULL)
+        input = str;
+
+    // Base Case
+    // when the input is empty, return input as NULL pointer to exit this function
+    if (input == NULL)
+        return NULL;
+
+    // this will be the output array
+    char *output = new char[strlen(input) + 1];
+
+    int i = 0;
+    for (; input[i] != '\0'; i++)
+    {
+        if (input[i] != DELIM)
+            output[i] = input[i];
+
+        else
+        {
+            // when token end is reached, transfer the input to the current position
+            // and mark this as '\0'.
+            output[i] = '\0';
+            input = input + i + 1;
+            // return the token
+            return output;
+        }
+    }
+
+    // corner cases
+    output[i] = '\0';
+    input = NULL;
+    return output;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
+
+    char input[] = "Today is a sunny day";
+    char *ptr = mystrtok(input, ' ');
+
+    while (ptr != NULL)
+    {
+        cout << ptr << endl;
+        ptr = mystrtok(NULL, ' ');
+    }
 }

fe6419c994a955d34a7e7b761a41cacb8afb2063
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-12-07 01:00:10 +0530

    cp

---
+++ LeetCode/December Contest/6.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define my_sizeof(type) ((char *)(&type + 1) - (char *)(&type))
+#define FOR(i, start, end) for (int i = start; i < end; i++)
+#define ll long long int
+#define ull unsigned long long int
+#define l long int
+#define ul unsigned long int
+#define start \
+    int t;    \
+    cin >> t; \
+    while (t--)
+const ll mod = 1000000007;
+
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node *_left, Node *_right, Node *_next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root == NULL)
+            return NULL;
+
+        if (root->left != NULL)
+            root->left->next = root->right;
+        connect(root->right);
+        connect(root->left);
+
+        return root;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+}

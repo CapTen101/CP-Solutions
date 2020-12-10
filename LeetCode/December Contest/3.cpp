77d3907a87cc451315c927d7ade2a70bcb431eca
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-12-10 22:52:19 +0530

    next question i progress

--- LeetCode/December Contest/3.cpp
+++ LeetCode/December Contest/3.cpp
@@ -27,23 +27,49 @@ struct TreeNode
 class Solution
 {
 public:
+    vector<int> list;
+
+    void *inorder(TreeNode *root)
+    {
+        if (root == NULL)
+            return root;
+
+        inorder(root->left);
+        list.push_back(root->val);
+        inorder(root->right);
+    }
+
     TreeNode *increasingBST(TreeNode *root)
     {
-        TreeNode *current_node = root;
-        TreeNode *newTreeRoot = new TreeNode();
-        TreeNode *current_new_node = new TreeNode();
-        current_new_node = newTreeRoot;
-        stack<TreeNode *> s;
+        // TreeNode *current_node = root;
+        // TreeNode *newTreeRoot = new TreeNode();
+        // TreeNode *current_new_node = new TreeNode();
+        // current_new_node = newTreeRoot;
+        // stack<TreeNode *> s;
 
-        while (current_node != NULL || s.empty() == false)
+        // while (current_node != NULL || s.empty() == false)
+        // {
+        //     s.push(current_node);
+        //     current_node = current_node->left; // go all the way to left bottom.
+        // }
+
+        // current_node = s.top();
+        // newTreeRoot->val = current_node->val;
+        // s.pop();
+
+        TreeNode *current = root;
+        inorder(root);
+
+        reverse(list.begin(), list.end());
+        int index = list.size() - 1;
+
+        while (list.empty() == false && index >= 0)
         {
-            s.push(current_node);
-            current_node = current_node->left; // go all the way to left bottom.
+            TreeNode *node = new TreeNode(list[index]);
+            node->right = new TreeNode(--index);
+            list.pop_back();
+            index--;
         }
-
-        current_node = s.top();
-        newTreeRoot->val = current_node->val;
-        s.pop();
     }
 };
 

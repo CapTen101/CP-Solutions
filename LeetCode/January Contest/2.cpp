6b56a1c67b2887405a9fca23d939d2f4579cd360
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2021-01-02 19:52:05 +0530

    January 2nd better approach

--- LeetCode/January Contest/2.cpp
+++ LeetCode/January Contest/2.cpp
@@ -26,6 +26,56 @@ class Solution
 public:
     TreeNode *getTargetCopy(TreeNode *original, TreeNode *cloned, TreeNode *target)
     {
+        // Below is 1.108 second solution:
+
+        // TreeNode *head = original;     // head of the original tree
+        // TreeNode *head_clone = cloned; // head of the cloned tree
+
+        // TreeNode *ans = NULL;
+
+        // stack<TreeNode *> s_orig;
+        // stack<TreeNode *> s_clone;
+        // s_orig.push(head);
+        // s_clone.push(head_clone);
+
+        // while (s_orig.empty() == false && s_clone.empty() == false)
+        // {
+        //     TreeNode *current = s_orig.top();
+        //     TreeNode *current_clone = s_clone.top();
+
+        //     if (current->val == target->val)
+        //     {
+        //         ans = current_clone;
+        //         return current_clone;
+        //     }
+
+        //     s_orig.pop();
+        //     s_clone.pop();
+
+        //     if (current->right != NULL)
+        //     {
+        //         s_orig.push(current->right);
+        //         s_clone.push(current_clone->right);
+        //     }
+        //     if (current->left != NULL)
+        //     {
+        //         s_orig.push(current->left);
+        //         s_clone.push(current_clone->left);
+        //     }
+        // }
+
+        // return ans;
+
+        if (original == target || original == NULL)
+            return cloned;
+
+        TreeNode *left = getTargetCopy(original->left, cloned->left, target);
+
+        if (left != NULL)
+            return left;
+
+        else
+            return getTargetCopy(original->right, cloned->right, target);
     }
 };
 
